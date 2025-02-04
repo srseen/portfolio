3cb26c3503cec4942fe96f552d3b83e9cde7c9af
srseen <seereen.2540@gmail.com>
2025-02-04 21:59:13 +0700

    add folder for data and edit skills components

---
+++ src/Data/skilldata.jsx
@@ -0,0 +1,44 @@
+const skillsData = [
+  {
+    category: "Frontend",
+    skills: [
+      { name: "HTML", description: "Markup language for creating web pages" },
+      {
+        name: "CSS",
+        description: "Style sheet language for designing web pages",
+      },
+      {
+        name: "JavaScript",
+        description: "Programming language for web development",
+      },
+      {
+        name: "React",
+        description: "JavaScript library for building user interfaces",
+      },
+    ],
+  },
+  {
+    category: "Backend",
+    skills: [
+      {
+        name: "Node.js",
+        description: "JavaScript runtime for server-side programming",
+      },
+      { name: "Express", description: "Web application framework for Node.js" },
+      {
+        name: "MongoDB",
+        description: "NoSQL database for modern applications",
+      },
+      {
+        name: "SQL",
+        description: "Language for managing relational databases",
+      },
+      {
+        name: "REST APIs",
+        description: "Architectural style for designing networked applications",
+      },
+    ],
+  },
+];
+
+export default skillsData;
