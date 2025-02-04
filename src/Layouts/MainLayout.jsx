713266704f3a8b3caf1a17cb9419f3ef2879b075
srseen <seereen.2540@gmail.com>
2025-02-04 16:42:01 +0700

    Add funtion theme mode in children

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,18 +1,31 @@
+import { useContext } from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import PropTypes from "prop-types";
+import ThemeContext from "../Contexts/ThemeContext";
 
 const MainLayout = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
       <div className="flex flex-col h-screen">
         <Navbar />
-        <main className="flex-grow">{children}</main>
+        <main
+          className={`${
+            theme === "light"
+              ? "bg-gradient-to-r from-blue-100 to-purple-100"
+              : "bg-gradient-to-r from-blue-500 to-purple-500"
+          } flex-grow flex items-center justify-center`}
+        >
+          {children}
+        </main>
         <Footer />
       </div>
     </>
   );
 };
+
 MainLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
