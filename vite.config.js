a7ccf9ddb20bba96ee3b9366a015c240dac20d6d
srseen <seereen.2540@gmail.com>
2025-02-10 17:16:04 +0700

    Add config for deploy with github

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ใช้ "/" สำหรับ root path หรือแก้ไขตาม domain ของคุณ
-  build: {
-    outDir: "dist",
-  },
 });
